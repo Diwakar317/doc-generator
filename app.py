2d71211a2d59113514f79c162be8e2fc6bd251ba
Diwakar317 <diwakar317@gmail.com>
2025-07-18 20:38:00 +0530

    gitignore removed

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def index():
     return render_template("index.html", flashcards=flashcards, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
